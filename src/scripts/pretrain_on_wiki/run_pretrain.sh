bb09bebbe84044866c11d8d8c0184ff207846ea3
michael <michael_wzhu91@163.com>
2020-05-11 13:18:28 +0800

    fairseq preprocess

--- src/scripts/pretrain_on_wiki/run_pretrain.sh
+++ src/scripts/pretrain_on_wiki/run_pretrain.sh
@@ -13,11 +13,12 @@ UPDATE_FREQ=16          # Increase the batch size 16x
 
 
 DATA_DIR=data-bin/zh_wiki
-USER_DIR=./mpnet_zh/src
+USER_DIR=mpnet_zh/src
 
-python3 /usr/share/torch-xla-1.5/tpu-examples/deps/fairseq/train.py $DATA_DIR \
+python3 train.py $DATA_DIR \
     --fp16 --user-dir $USER_DIR \
-    --task masked_permutation_lm --criterion masked_permutation_cross_entropy \
+    --task masked_permutation_lm \
+    --criterion masked_permutation_cross_entropy \
     --arch mpnet_base --sample-break-mode complete --tokens-per-sample $TOKENS_PER_SAMPLE \
     --optimizer adam --adam-betas '(0.9,0.98)' --adam-eps 1e-6 --clip-norm 0.0 \
     --lr-scheduler polynomial_decay --lr $PEAK_LR --warmup-updates $WARMUP_UPDATES --total-num-update $TOTAL_UPDATES \
